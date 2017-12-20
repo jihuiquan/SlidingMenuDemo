fdc8e8f029dba554db93e75aaf0065b53042901b
jihuiquan <huiquan_ji@ssic.cn>
2017-12-20 11:20:30 +0800

    commit

---
+++ SMenuDemo/app/src/main/java/smenudemo/cn/ssic/smenudemo/MainActivity.java
@@ -0,0 +1,16 @@
+package smenudemo.cn.ssic.smenudemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author JHQ
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
